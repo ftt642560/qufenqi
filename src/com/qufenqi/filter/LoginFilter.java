639cc42f9fe6d2089f0f08de4eb089969a6d1d17
ftt <1254171906@qq.com>
2015-11-26 16:05:18 +0800

    添加了过滤器，除了login.jsp外当没有登录时不能通过url访问jsp页面
    Signed-off-by: ftt <1254171906@qq.com>

---
+++ src/com/qufenqi/filter/LoginFilter.java
@@ -0,0 +1,50 @@
+package com.qufenqi.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.qufenqi.entity.User;
+
+public class LoginFilter extends HttpServlet implements Filter{
+	@Override
+	public void destroy() {
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+			FilterChain filterChain) throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) servletRequest;
+		HttpServletResponse response = (HttpServletResponse) servletResponse;
+		HttpSession session = request.getSession();
+		String url = request.getServletPath();
+		String contextPath = request.getContextPath();
+		if(url.equals("")){
+			url+="/";
+		}
+		if((url.startsWith("/")&&!url.startsWith("/login"))){//若访问后台资源 过滤到login    
+			User user = (User) session.getAttribute("user");
+			if(user == null){
+				response.sendRedirect(contextPath+"/login.jsp");
+				return;
+			}
+		}
+		filterChain.doFilter(request, response);
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		
+	}
+
+}
